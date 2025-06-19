d877aa27f8c51f1743e96444c8862e66a728e79d
chamdarig <109315064+chamdarig@users.noreply.github.com>
2025-06-19 23:30:48 +0200

    Create script.js

---
+++ archimedes/script.js
@@ -0,0 +1,13 @@
+// a square + b square = c square
+
+// get value of a from user and store it in variable a
+a = prompt("provide a")
+
+// get value of b from user and store it in variable b
+b = prompt("provide b");
+
+//square values
+c = (a*2 + b*2) *2;
+
+//print the results
+console.log(`entered a: ${a} and entered b: ${b} and it's c = ${c}`);
